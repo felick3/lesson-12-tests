02c82b6b37df07126ea57231a968df6580d8152a
Дарья <fkjdskfkf32@mail.ru>
2024-12-08 13:32:14 +0300

    1

---
+++ hand_tests.py
@@ -0,0 +1,17 @@
+from functions import *
+if salary != 20:
+    print('Error')
+if hello_who('Max') != 'Hello,Max':
+    print('Failed')
+else:
+    print('Good')
+if hello_who('Leo') != 'Hello,Leo':
+    print('Failed')
+else:
+    print('Amazing')
+if salary == 0:
+    print('Уволен')
+if hello_who('1') != 'Hello,1':
+    print('Failed')
+else:
+    print('1')
